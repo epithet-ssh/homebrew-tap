1b274bc73a446b29773d3b79e0187412b5f7b204
Brian McCallister <brianm@skife.org>
2025-11-28 11:50:37 -0800

    homebrew has gotten really opinionated :-(

--- Formula/epithet.rb
+++ Formula/epithet.rb
@@ -1,12 +1,10 @@
 class Epithet < Formula
   desc "SSH certificate management"
   homepage "https://epithet.dev/"
-
-  head "https://github.com/epithet-ssh/epithet.git"
   url "https://github.com/epithet-ssh/epithet/archive/refs/tags/v0.1.5.tar.gz"
-
   sha256 "17d3668763a39eed809c10e09786f9c5edf2a9059dd87884003eb2abc6fe4f3e"
   license "Apache-2.0"
+  head "https://github.com/epithet-ssh/epithet.git"
 
   depends_on "go" => :build
 
