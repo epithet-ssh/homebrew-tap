16bfeafbef4e8f85de507ba2a005821ed4c409ee
Brian McCallister <brianm@skife.org>
2025-11-29 11:43:54 -0800

    update to 0.1.6 and add brew service to start epoithet

--- Formula/epithet.rb
+++ Formula/epithet.rb
@@ -1,8 +1,8 @@
 class Epithet < Formula
   desc "SSH certificate management"
   homepage "https://epithet.dev/"
-  url "https://github.com/epithet-ssh/epithet/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "17d3668763a39eed809c10e09786f9c5edf2a9059dd87884003eb2abc6fe4f3e"
+  url "https://github.com/epithet-ssh/epithet/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "1be012d082f5f2ca2a7e3ac042dc52438e91a0bf0ffa92f3f52e22ca94b5085c"
   license "Apache-2.0"
   head "https://github.com/epithet-ssh/epithet.git"
 
@@ -11,6 +11,22 @@ class Epithet < Formula
   def install
     system "make", "epithet"
     bin.install "epithet"
+    (share/"epithet").install "examples/client/.epithet/config" => "config.example"
+  end
+
+  def post_install
+    (Dir.home/".epithet").mkpath
+    (Pathname.new(Dir.home)/"Library/logs/dev.epithet").mkpath
+
+    config_example = share/"epithet/config.example"
+    target = Pathname.new(Dir.home)/".epithet/config.example"
+    cp config_example, target unless target.exist?
+  end
+
+  service do
+    run [opt_bin/"epithet", "--log-file=#{Dir.home}/Library/logs/dev.epithet/agent.log", "agent"]
+    keep_alive true
+    process_type :background
   end
 
   test do
