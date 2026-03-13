7c6a5314f653613a73564956e3cbd2173f3b5a8f
Brian McCallister <brianm@skife.org>
2026-03-12 19:55:06 -0700

    update epithet

--- Formula/epithet.rb
+++ Formula/epithet.rb
@@ -1,26 +1,26 @@
 class Epithet < Formula
   desc "SSH certificate management"
   homepage "https://epithet.dev/"
-  version "0.9.0"
+  version "0.12.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/epithet-ssh/epithet/releases/download/v0.9.0/epithet_0.9.0_darwin_arm64.tar.gz"
-      sha256 "8b8be6a2b972c8d121b70c1ed94ffa5e8319c3829c1aae6ec9d71121b374e1f6"
+      url "https://github.com/epithet-ssh/epithet/releases/download/v0.12.0/epithet_0.12.0_darwin_arm64.tar.gz"
+      sha256 "74fae2f6268f82e29fe3a11d1131f2f3f7aaa78704e096f49eb640c40289c674"
     else
-      url "https://github.com/epithet-ssh/epithet/releases/download/v0.9.0/epithet_0.9.0_darwin_amd64.tar.gz"
-      sha256 "fdd130eb218d9cfc1eb3390039a25f7335f7ec82d78e0384eb2de3e3c7e02556"
+      url "https://github.com/epithet-ssh/epithet/releases/download/v0.12.0/epithet_0.12.0_darwin_amd64.tar.gz"
+      sha256 "eee283dc9f53b2b68ec2917c20f0b635b928a084cdc9a3b2fe5f01c6e4298fd6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/epithet-ssh/epithet/releases/download/v0.9.0/epithet_0.9.0_linux_arm64.tar.gz"
-      sha256 "adfd32a0ea57da9e2bf396ae506d9e5cfb2174ba3d990904f813d1f600263265"
+      url "https://github.com/epithet-ssh/epithet/releases/download/v0.12.0/epithet_0.12.0_linux_arm64.tar.gz"
+      sha256 "5d5157d59fd1c6a4b219533318fe05191de0e8fe663ec1e57520294ec16332a4"
     else
-      url "https://github.com/epithet-ssh/epithet/releases/download/v0.9.0/epithet_0.9.0_linux_amd64.tar.gz"
-      sha256 "f287b75e363b634a904bdaa9dbccd2fec142ac5dc17ef3f34f5e18cdcd1d1d3b"
+      url "https://github.com/epithet-ssh/epithet/releases/download/v0.12.0/epithet_0.12.0_linux_amd64.tar.gz"
+      sha256 "8d3b83d6ad9ccef7d8af4b27dc39c373c0f038097e852057ae64d580953fc6fc"
     end
   end
 
